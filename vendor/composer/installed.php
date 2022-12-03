cd878e107fc501d570a04c220915297a53aad8ba
Amila Shanka Gunashinha <dsa.amilashanaka@gmail.com>
2022-12-03 08:22:34 +0000

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gmaccario/simple-mvc-php-framework',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3a5a823bc98844a7b6930e4fcfb43a5a5e581b71',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gmaccario/simple-mvc-php-framework' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3a5a823bc98844a7b6930e4fcfb43a5a5e581b71',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
